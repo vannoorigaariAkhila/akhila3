5b9545585279365c345ab87b2f5801622bf78987
vannoorigaariAkhila <vakhila573@gmail.com>
2024-07-07 11:21:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import Welcome from "./Welcome";
+function App(){
+  return <Welcome name='Kavya' gender='female'/>
+
+}
+export default App;
---
+++ Welcome.js
@@ -0,0 +1,4 @@
+function Welcome(props){
+    return <h1>{props.name}<br></br>{props.gender}<br></br> Welcome Function</h1>
+  }
+  export default Welcome;
